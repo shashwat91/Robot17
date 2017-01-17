594ffe3451fb5b86e3add84ef888eab15d40720e
ishugoel20 <ishugoel20@gmail.com>
2017-01-17 14:45:25 +0100

    changes in ros opencv file

--- ros_opencv/src/ros_opencv.cpp
+++ ros_opencv/src/ros_opencv.cpp
@@ -1,13 +1,30 @@
 
-#include "cv.h" 
- #include "highgui.h" 
- #include <stdio.h>
+#include <stdio.h>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+void chatterCallback(const sensor_msgs::ImageConstPtr& image)
+{
+   //ROS_INFO("I heard: [%s]", msg->data.c_str());
+   cv::NamedWindow("window1");
+   imshow("ROS Image", image);
+   waitKey(0);
+}
 
 
 int main()
 {
-CvCapture* capture = cvCaptureFromCAM(0);
-IplImage* frame;
+
+ros::init(argc, argv, "listener");
+ros::NodeHandle n;
+
+ros::Subscriber sub = n.subscribe("/camera/image", 1, chatterCallback);
+//CvCapture* capture = cvCaptureFromCAM(0);
+/*IplImage* frame;
 cvNamedWindow("window1");
 while(1)
 {
@@ -17,7 +34,8 @@ cvWaitKey(1);
 //cvSaveImage("image1.png",frame);
 }
 cvReleaseImage(&frame);
-cvReleaseCapture( &capture );
- cvDestroyWindow( "window1" );
+cvReleaseCapture( &capture ); */
+ros::spin();
+ cv::DestroyWindow( "window1" );
 return 0;
 }
